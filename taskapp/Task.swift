b6cb50239a5cbcf32d13c242490679107f560b6e
gggkosaka <butakimutii@gmail.com>
2020-10-16 16:55:51 +0900

    「課題：タスク管理アプリの機能追加」に対応しました。 ※発展課題は未実施です。

---
+++ taskapp/Task.swift
@@ -0,0 +1,32 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by gkosaka on 2020/10/14.
+//  Copyright © 2020 gggkosaka. All rights reserved.
+//
+
+// モデルクラス
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+
+    // タイトル
+    @objc dynamic var title = ""
+
+    // 内容
+    @objc dynamic var contents = ""
+
+    // 日時
+    @objc dynamic var date = Date()
+
+    // カテゴリ
+    @objc dynamic var category = ""
+
+    // id をプライマリーキーとして設定
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
